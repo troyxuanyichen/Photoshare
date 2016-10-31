4a94ace67fe43b78ccaabcf3b7e20036b26c7c2f
Xuanyi Chen <troychen@bu.edu>
2016-10-31 14:13:47 -0400

    finish photo upload with a fake album id 1, use trigger

--- controller/photoController.py
+++ controller/photoController.py
@@ -18,7 +18,7 @@ def getPhotosFromAlbum(aid):
 def getIdFromData(data):
 	conn = connection.init_connection()
 	cursor = conn.cursor()
-	if cursor.execute("SELECT albumId FROM photos WHERE data ='{0}".format(data)):
+	if cursor.execute("SELECT albumId FROM photos WHERE data ='{0}'".format(data)):
 		photoId = cursor.fetchone()[0]
 		return photoId
 	else:
@@ -39,21 +39,5 @@ def uploadPhoto(aid, caption, data):
 	if cursor.execute("INSERT INTO photos (albumId, caption, data) VALUES ('{0}', '{1}', '{2}')".format(aid, caption, data)) != 1:
 		conn.rollback()
 		return False
-	photoId = getIdFromData(data) if getIdFromData(data) else False
-	if photoId == False:
-		conn.rollback()
-		return False
-	
 	conn.commit()
-
-	# cursor.execute("SELECT data FROM photos WHERE photoId = '{0}'".format(1))
-	# photo = cursor.fetchone()[0]
-	# wtf = cursor.execute("INSERT INTO photos (albumId, caption, data) VALUES ('{0}', '{1}', '{2}')".format(aid, caption, data))
-	# print wtf
-	# if failed:
-	# 	conn.rollback()
-	# else:
-	# 	cursor.execute("INSERT INTO photos () VALUES data, photoId FROM photos WHERE albumId = '{0}".format(aid))
-	# conn.commit()
-
-
+	return True
